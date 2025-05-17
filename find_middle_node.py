c090293d2990388dc87dc9442146aee7d961f2d2
Kritika Gupta <162892864+kritu08231@users.noreply.github.com>
2025-05-17 22:00:01 +0530

    Create find_middle_node.py

---
+++ find_middle_node.py
@@ -0,0 +1,80 @@
+class node:
+    def __init__(self,value):
+        self.val = value
+        self.next = None
+class LinkedList:
+    head = None
+
+    def printLinkedList(self):
+        temp = self.head
+        while temp != None:
+            print(temp.val , end=" ")
+            temp = temp.next
+        print()
+            
+    def insertionAtTail(self, value):
+        if self.head == None:
+            self.head = node(value)
+            return
+        temp = self.head
+        while temp.next != None:
+            temp = temp.next
+        temp.next = node(value)
+    
+    def insertionAtHead(self,value):
+        temp = node(value)
+        temp.next = self.head
+        self.head = temp
+    
+    def insertion(self,value,k):
+        if k ==0:
+            insertionAtHead(value)
+            return
+        
+        count = 2
+        temp = self.head
+        while count < k and temp != None:
+            count +=1
+            temp = temp.next
+        if count <k:
+            print('k is grater than len')
+            return
+        rest = temp.next
+        temp.next = node(value)
+        temp.next.next = rest
+    
+    def deletion(self,k):
+        if k == 1:
+            self.head = self.head.next
+            return
+        count = 2
+        temp = self.head
+        while count < k and temp != None:
+            count +=1
+            temp = temp.next
+        if count < k:
+            print("K is grater than len")
+            return
+        temp.next = temp.next.next
+    
+    def middle(self):
+        slow = self.head
+        fast = self.head
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+linkedList = LinkedList()
+linkedList.insertionAtTail(1)
+linkedList.insertionAtTail(2)
+linkedList.insertionAtTail(3)
+linkedList.insertionAtHead(0)
+linkedList.insertion(5,3)
+linkedList.printLinkedList()
+linkedList.deletion(3)
+linkedList.deletion(2)
+linkedList.printLinkedList()
+middleNode = linkedList.middle()
+print(middleNode.val)
+
